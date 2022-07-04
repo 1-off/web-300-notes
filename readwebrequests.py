3673605383a0d2d491849dbb4f6bf24e1dcd33d6
1-off <97915378+1-off@users.noreply.github.com>
2022-07-04 19:58:48 +0200

    Update readwebrequests.py

--- readwebrequests.py
+++ readwebrequests.py
@@ -3,6 +3,8 @@ from colorama import Fore, Back, Style
 requests.packages.urllib3.\
 disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
+proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
+
 def format_text(title,item):
   cr = '\r\n'
   section_break = cr +  "*" * 20 + cr
@@ -10,7 +12,7 @@ def format_text(title,item):
   text = Style.BRIGHT + Fore.RED + title + Fore.RESET + section_break + item + section_break
   return text
 
-r = requests.get('https://manageengine:8443/',verify=False)
+r = requests.get('https://manageengine:8443/',verify=False, proxies=proxies)
 print(format_text('r.status_code is: ',r.status_code))
 print(format_text('r.headers is: ',r.headers))
 print(format_text('r.cookies is: ',r.cookies))
